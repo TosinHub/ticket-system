e10c94f87b164c0d5ecdf53a0732ddc0588cbd58
TosinHub <tosinolugbenga@gmail.com>
2023-01-25 12:01:50 +0000

    Test

--- test/__test.js
+++ test/__test.js
@@ -1,6 +1,6 @@
+import TicketService from "../src/pairtest/TicketService";
 
-
-const ticketService = new TicketService(20);
+const ticketService = new TicketService();
 
 describe('TicketService', () => {
     beforeEach(() => {
