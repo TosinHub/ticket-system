822d42f729e6361b4e55163b9fe80a6f09b9327e
TosinHub <tosinolugbenga@gmail.com>
2023-01-25 11:44:58 +0000

    commit

---
+++ src/pairtest/TicketService.js
@@ -0,0 +1,50 @@
+import TicketTypeRequest from './lib/TicketTypeRequest.js';
+import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway'
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
+
+export default class TicketService {
+  /**
+   * Should only have private methods other than the one below.
+   */
+
+  purchaseTickets(accountId, ...ticketTypeRequests) {
+    // throws InvalidPurchaseException
+   
+    const ticketTypeRequest = new TicketTypeRequest(...ticketTypeRequests, 20)
+    const {INFANT, CHILD, ADULT } = ticketTypeRequest.getTicketType()
+    
+    
+    const totalTicket = INFANT + CHILD + ADULT
+
+    if( totalTicket > ticketTypeRequest.getNoOfTickets() ){
+      throw new TypeError('Error: Maximum limit of 20 tickets');
+    }
+
+    if(CHILD > 0 || INFANT > 0) {
+      if(ADULT === 0 ){
+        throw new TypeError('Error : CHILD and INFANT ticket cannot be purchased without an ADULT ticket ');
+      }
+    }
+
+    const totalPrice = (CHILD * 10) + (ADULT * 20);
+    const makePayment = new TicketPaymentService()
+    const payment = makePayment.makePayment(accountId, totalPrice)
+
+    //assuming the payment gateway returns a success or failure message
+    if(payment.status === 'success'){
+      const seatToResearve = CHILD + ADULT;
+      const seatReservation = new SeatReservationService()
+      const reservation = seatReservation.reserveSeat(accountId, seatToResearve)
+      //Assuming the Seat Reservation returns a status of success or failure
+      if(reservation.status == 'success'){
+        return 'Purchase Successful'
+      }else{
+        return 'Error: seat reservation failed'
+      }
+    }else{
+      return 'Error: payment failed'
+    }
+
+  }
+}
---
+++ test/__test.js
@@ -0,0 +1,25 @@
+
+
+const ticketService = new TicketService(20);
+
+describe('TicketService', () => {
+    beforeEach(() => {
+        jest.spyOn(TicketPaymentService, 'makePayment').mockReturnValue({status: 'success'});
+        jest.spyOn(SeatReservationService, 'makeReservation').mockReturnValue({status: 'success'});
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    describe('purchaseTickets', () => {
+        test('should return error if the total number of tickets requested would exceed the maximum number of tickets that can be sold', () => {
+            const result = ticketService.purchaseTickets({infant: 1, child: 10, adult: 10}, 1);
+            expect(result).toEqual('Error: Maximum limit of 20 tickets exceeded');
+        });
+        test('should return error if request is not valid', () => {
+            const result = ticketService.purchaseTickets({infant: 1, child: 1}, 1);
+
+        })
+
+    })
+
+})
